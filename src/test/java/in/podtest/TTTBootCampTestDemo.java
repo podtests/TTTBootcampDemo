dbd7108322dad1d7eadf0d958ca47c95ea6c0e75
Akhiljda <38800897+Akhiljda@users.noreply.github.com>
2024-02-04 12:33:03 +0530

    Add sout line

--- src/test/java/in/podtest/TTTBootCampTestDemo.java
+++ src/test/java/in/podtest/TTTBootCampTestDemo.java
@@ -70,6 +70,8 @@ public class TTTBootCampTestDemo {
 
         item.fillQuantity("2").selectSize("L").selectColor("Green");
 
+        System.out.println("Selected the item");
+
         Thread.sleep(4000);
 
         item.clickAddToCart();
